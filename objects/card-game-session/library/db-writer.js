e998162a80fac10f28c13db90dd32bb2be676d49
Jo-Neto <jcb.oliveira.neto@gmail.com>
2022-03-22 23:05:11 -0300

    update

--- objects/card-game-session/library/db-writer.js
+++ objects/card-game-session/library/db-writer.js
@@ -5,31 +5,54 @@ const fs = require('fs');
 
 //function stores session on database and sends message to players who have an OPEN socket
 
-//TODO: don't send message to closed/inexistent sockets
-
 module.exports = function (Session, winner) { //IMPORTANT, ONLY ACCEPTS 'draw', 'p1' OR 'p2' STRING TYPES, LOWERCASE
     console.log("CardGameSession object --> storeOnDatabase(fn) --> SessionNum:" + Session.sID);
-    Session.player1.ws.send(JSON.stringify(Session.gameState));
-    Session.player2.ws.send(JSON.stringify(Session.gameState));
-    if (winner === 'p1') { 
-        Session.player1.ws.send("Voce ganhou!");
-        Session.player2.ws.send("Voce perdeu");
+    
+    if (Session.player1.ws.readyState === 1)
+        Session.player1.ws.send(JSON.stringify(Session.gameState));
+    
+    if (Session.player2.ws.readyState === 1)
+        Session.player2.ws.send(JSON.stringify(Session.gameState));
+    
+    if (winner === 'p1') {  
+        if (Session.player1.ws.readyState === 1)    
+            Session.player1.ws.send("Voce ganhou!");
+        if (Session.player2.ws.readyState === 1)
+            Session.player2.ws.send("Voce perdeu");
         //TODO: check account and give points
-    } else if (winner === 'p2') {
-        Session.player1.ws.send("Voce perdeu");
-        Session.player2.ws.send("Voce ganhou!");
+    } 
+    
+    else if (winner === 'p2') {
+        if (Session.player1.ws.readyState === 1)
+            Session.player1.ws.send("Voce perdeu");
+        if (Session.player2.ws.readyState === 1)
+            Session.player2.ws.send("Voce ganhou!");
         //TODO: check account and give points
-    } else if (winner === 'draw') {
-        Session.player1.ws.send("Empate!");
-        Session.player2.ws.send("Empate!");
-    } else
-        console.log("CardGameSession object --> storeOnDatabase(fn) --> INVALID WINNER STRING --> SessionNum: " + Session.sID);
-    Session.player1.ws.close(1000, 'match has finished'); //close socket and warn front that everything is fine
-    Session.player2.ws.close(1000, 'match has finished');
-    Session.player1.ws.terminate(); //safety 
-    Session.player2.ws.terminate(); //safety
-    Session.player1.key = null; //clear reconnection keys
-    Session.player2.key = null;
+    } 
+    
+    else if (winner === 'draw') {
+        if (Session.player1.ws.readyState === 1) 
+            Session.player1.ws.send("Empate!");
+        if (Session.player2.ws.readyState === 1)
+            Session.player2.ws.send("Empate!");
+    } 
+    
+    else
+        console.log("CardGameSession object --> storeOnDatabase(fn) --> INVALID WINNER STRING --> SessionNum: " + Session.sID); 
+    
+    if (Session.player1.ws.readyState === 1 || Session.player1.ws.readyState === 0) {
+       Session.player1.ws.close(1000, 'match has finished'); //close socket and warn front that everything is fine  
+       Session.player1.ws.terminate(); //safety
+    } 
+    if (Session.player2.ws.readyState === 1 || Session.player2.ws.readyState === 0) {
+        Session.player2.ws.close(1000, 'match has finished');
+        Session.player2.ws.terminate(); //safety
+    } 
+
+    Session.player1.reconKey = null; //clear reconnection keys
+    Session.player2.reconKey = null;
+    
+    
     fs.readFile('./database/game-sessions.json', (err, readData) => { //save match data on database
         if (err) { console.log("ERROR: SessionNum:" + Session.sID + "on reading database: "); throw console.log(err); }
         let dataBase = JSON.parse(readData);
@@ -47,9 +70,9 @@ module.exports = function (Session, winner) { //IMPORTANT, ONLY ACCEPTS 'draw', 
                 account: Session.player2.account,
             },
             turnNum: Session.gameState.turnNum,
-            disconnec: Session.gameState.disconnec,
-            hasGivenUp: Session.gameState.hasGivenUp,
-            hasCheated: Session.gameState.hasCheated
+            disconnec: Session.serverSide.disconnec,
+            hasGivenUp: Session.serverSide.hasGivenUp,
+            hasCheated: Session.serverSide.hasCheated
         });
         let toWrite = JSON.stringify(dataBase);
         fs.writeFile('./database/game-sessions.json', toWrite, (err, out) => {
@@ -58,4 +81,5 @@ module.exports = function (Session, winner) { //IMPORTANT, ONLY ACCEPTS 'draw', 
         console.log("CardGameSession object --> storeOnDatabase(fn) --> session num: " + Session.sID + ' registered on database');
         Session = null; //nullify session for it to be replaced on the active game sessions array
     });
+
 }
